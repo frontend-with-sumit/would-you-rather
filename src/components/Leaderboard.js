437c8eb8fe88385b14e73cc4abd0ce928539801b
Sumit <sk990005@outlook.com>
2019-10-15 13:36:24 +0530

    Added Leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Leaderboard extends Component {
   render() {
-    return <div className="container mt-10">Leaderboard</div>;
+    const { userIds, users } = this.props;
+
+    console.log(users);
+    return (
+      <div className="container mt-10">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Questions Created</th>
+              <th>Answered Questions</th>
+              <th>Total Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userIds.map(id => (
+              <tr key={id}>
+                <td>{users[id].name}</td>
+                <td>{users[id].questions.length}</td>
+                <td>{Object.keys(users[id].answers).length}</td>
+                <td>
+                  {users[id].questions.length +
+                    Object.keys(users[id].answers).length}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
-export default Leaderboard;
+function mapStateToProps({ users }) {
+  return {
+    users,
+    userIds: Object.keys(users)
+  };
+}
+export default connect(mapStateToProps)(Leaderboard);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middleware";
 
-const store = createStore(reducer, middleware);
+const store = createStore(
+  reducer,
+  compose(
+    middleware,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
